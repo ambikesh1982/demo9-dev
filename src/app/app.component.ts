ada15882e332734255c451c8cd959af210967fa8
ambikesh1982 <ambikesh1982@gmail.com>
2019-09-05 23:06:04 -0400

    snackbar works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './core/auth.service';
 import { Event, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { AppUser } from './core/models';
+import { SnackbarNotificationService } from './core/snackbar-notification.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   // title = 'Yummz9*';
   title = null;
   isLoading: boolean;
+  currUser$: Observable<AppUser>;
 
-  constructor(public auth: AuthService, private router: Router) {
+  constructor(public auth: AuthService, private router: Router, private notify: SnackbarNotificationService) {
     this.isLoading = false;
     this.router.events.subscribe((e: Event) => {
       this.checkRouterEvent(e);
@@ -30,5 +35,11 @@ export class AppComponent {
     ) {
       this.isLoading = false;
     }
+  }
+
+  ngOnInit() {
+    this.currUser$ = this.auth.currUser$.pipe(
+      tap(user => this.notify.openSnackBar(`Welcome ${user.displayName} !`))
+    );
   }
 }
