8d918ed06f4780b53aa09956d7dfae3ef3049c5a
ambikesh1982 <ambikesh1982@gmail.com>
2019-09-06 22:54:38 -0400

    conditional routing works

--- src/app/kitchen/kitchen-list/kitchen-list.component.ts
+++ src/app/kitchen/kitchen-list/kitchen-list.component.ts
@@ -3,6 +3,9 @@ import { KitchenService } from '../kitchen.service';
 import { Observable } from 'rxjs';
 import { IKitchen } from '../kitchen';
 import { NavigationComponent } from '../../navigation/navigation.component';
+import { AuthService } from 'src/app/core/auth.service';
+import { AppUser } from 'src/app/core/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kitchen-list',
@@ -11,10 +14,25 @@ import { NavigationComponent } from '../../navigation/navigation.component';
 })
 export class KitchenListComponent implements OnInit {
   kitchens$: Observable<IKitchen[]>;
-  constructor(private ks: KitchenService) { }
+  currentUser: AppUser;
+
+  constructor(private ks: KitchenService, private auth: AuthService, private router: Router) {
+    this.currentUser = this.auth.currUser;
+   }
 
   ngOnInit() {
     this.kitchens$ = this.ks.getKitchenList();
+  }
+
+  navigateTo(kitchenId: string, ownerId: string) {
+    console.log('kitchenId:ownerId', kitchenId, ' - ', ownerId);
+    if (ownerId === this.currentUser.uid) {
+      this.router.navigate(['my-kitchen/', kitchenId]);
+    } else {
+      this.router.navigate([kitchenId]);
+    }
+    // if (this.currentUser.uid === )
+    // [kitchen.ownerId == currentUser.uid ? 'my-kitchen/', user.kitchenId : kitchen.id]
   }
 
 }
